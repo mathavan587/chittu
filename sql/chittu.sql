27e695f37e267328c9e393b74780d585e727245d
mathavan-dev <mathavan2004v@gmail.com>
2025-04-16 08:07:50 +0530

    16-04-25

--- sql/chittu.sql
+++ sql/chittu.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 15, 2025 at 07:07 PM
+-- Generation Time: Apr 16, 2025 at 04:35 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.0.30
 
@@ -20,6 +20,56 @@ SET time_zone = "+00:00";
 --
 -- Database: `chittu`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `logs`
+--
+
+CREATE TABLE `logs` (
+  `id` int(11) NOT NULL,
+  `email` varchar(500) NOT NULL,
+  `mobile` varchar(15) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `logs`
+--
+
+INSERT INTO `logs` (`id`, `email`, `mobile`, `created_at`) VALUES
+(1, 'vmathavan587@gmail.com', '8148668359', '2025-04-15 20:14:13'),
+(2, 'vmathavan587@gmail.com', '8148668359', '2025-04-15 20:21:08'),
+(3, 'vmathavan587@gmail.com', '8148668359', '2025-04-15 20:22:18'),
+(4, 'vmathavan587@gmail.com', '8148668359', '2025-04-15 23:46:33'),
+(5, 'vmathavan587@gmail.com', '8148668359', '2025-04-15 23:46:59'),
+(6, 'vmathavan587@gmail.com', '8148668359', '2025-04-15 23:47:33'),
+(7, 'vmathavan587@gmail.com', '8148668359', '2025-04-15 23:48:35'),
+(8, 'vmathavan587@gmail.com', '8148668359', '2025-04-15 23:49:42'),
+(9, 'vmathavan587@gmail.com', '8148668359', '2025-04-15 23:50:15'),
+(10, 'vmathavan587@gmail.com', '8148668359', '2025-04-15 23:54:28'),
+(11, 'vmathavan587@gmail.com', '8148668359', '2025-04-15 23:54:47');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `services`
+--
+
+CREATE TABLE `services` (
+  `id` int(11) NOT NULL,
+  `service_id` int(11) NOT NULL,
+  `name` varchar(255) DEFAULT NULL,
+  `category` varchar(255) DEFAULT NULL,
+  `rate` decimal(10,2) DEFAULT NULL,
+  `min` int(11) DEFAULT NULL,
+  `max` int(11) DEFAULT NULL,
+  `type` varchar(50) DEFAULT NULL,
+  `desc` text DEFAULT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -46,11 +96,42 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `password`, `mobile`, `status`, `otp`, `usertype`, `is_deleted`, `modified_at`, `created_at`) VALUES
-(1, 'mathavan', 'vmathavan587@gmail.com', 'bdab5090f4448bcc378ad82f343af66d', '8148668359', 1, '418008', 'user', 0, '2025-04-15', '2025-04-15');
+(12, 'mathavan', 'vmathavan587@gmail.com', 'edfde2e93f66f7310c532e3d8432614b', '8148668359', 0, '48167179', 'user', 0, '2025-04-15', '2025-04-15');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `verification`
+--
+
+CREATE TABLE `verification` (
+  `id` int(11) NOT NULL,
+  `email` varchar(500) NOT NULL,
+  `otp` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `verification`
+--
+
+INSERT INTO `verification` (`id`, `email`, `otp`) VALUES
+(10, 'vmathavan587@gmail.com', 48167179);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `logs`
+--
+ALTER TABLE `logs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `services`
+--
+ALTER TABLE `services`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -61,14 +142,39 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `mobile` (`mobile`);
 
 --
+-- Indexes for table `verification`
+--
+ALTER TABLE `verification`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `email` (`email`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `logs`
+--
+ALTER TABLE `logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `services`
+--
+ALTER TABLE `services`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+
+--
+-- AUTO_INCREMENT for table `verification`
+--
+ALTER TABLE `verification`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
